2cc85ecc025f1274160a2fe3a28d49d48fc07cfc
alexeyz <alexey@rightscale.com>
2013-03-15 10:57:42 +0800

    metadata for RS

--- cookbooks/rs-rabbitmq/metadata.rb
+++ cookbooks/rs-rabbitmq/metadata.rb
@@ -5,3 +5,12 @@ license          'All rights reserved'
 description      'Installs/Configures rs-rabbitmq'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "rabbitmq", ">= 1.8.1"
+
+recipe            "rs-rabbitmq::default", "Install and configure RS-RabbitMQ"
+#recipe            "rs-rabbitmq::cluster", "Set up RabbitMQ clustering."
+#recipe            "rs-rabbitmq::plugin_management", "Manage plugins with node attributes"
+#recipe            "rs-rabbitmq::virtualhost_management", "Manage virtualhost with node attributes"
+#recipe            "rs-rabbitmq::user_management", "Manage users with node attributes"
+
